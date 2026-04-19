c57f605d838935f2d4c0e8d359a17c3794fb6656
MariaRafaela <mariaanjos2@discente.ufg.br>
2026-04-19 11:35:22 -0300

    Criação de requisições automáticas

--- client.py
+++ client.py
@@ -1,19 +1,66 @@
 from socket  import *
 from constCS import * #-
+import random
+import time
+NUM_REQUISICOES = 100000
+OPERACOES = ["sum", "sub", "mul"]
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-#s.send(str.encode('Hello, world'))  # send some data
+tempo_total = 0
+def gerar_requisicao():
+    #Gera uma requisição aleatória no formato '<op> <n1> <n2>'.
+    op = random.choice(OPERACOES)
+    n1 = round(random.uniform(-100, 100), 2)
+    n2 = round(random.uniform(-100, 100), 2)
+    return f"{op} {n1} {n2}"
 
-print ("Insira os dados para operação desejada (sum, sub ou mul) seguindo o padrão:\n<operacao> <n1> <n2>.\nQuando quiser encerrar, digite 'fim'\n")
+def modo_manual():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((HOST, PORT)) # connect to server (block until accepted)
+    #s.send(str.encode('Hello, world'))  # send some data
 
-while True:
-    msg= input ("")
+    print ("Insira os dados para operação desejada (sum, sub ou mul) seguindo o padrão:\n<operacao> <n1> <n2>.\nQuando quiser encerrar, digite 'fim'\n")
 
-    if msg.lower() == "fim": break
+    while True:
+        msg= input ("")
 
-    s.send(msg.encode())
-    data = s.recv(1024)     # receive the response
+        if msg.lower() == "fim": break
+
+        s.send(msg.encode())
+        data = s.recv(1024)     # receive the response
+        
+        print ("Resultado:", data.decode())            # print the result
+    s.close()     
+
+def modo_automatico():
+    global tempo_total
+    tempo_total = 0
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    for i in range(NUM_REQUISICOES):
+        msg = gerar_requisicao()
+        start = time.perf_counter()
+        s.send(msg.encode())
+        data = s.recv(1024) 
+        end = time.perf_counter()
+        tempo_resposta = end-start
+        tempo_total += tempo_resposta
+        #print ("Resultado:", data.decode(), f"Duracao: {(tempo_resposta*1000):.3f}")            # print the result
     
-    print ("Resultado:", data.decode())            # print the result
-s.close()               # close the connection
+    print(f"Tempo total: {(tempo_total*1000)} Tempo médio: {tempo_total*1000/NUM_REQUISICOES}")
+
+
+
+if __name__ == "__main__":
+
+    print("Escolha o modo:")
+    print("  1- Automático (gerador aleatório + paralelo)")
+    print("  2- Manual (fornecer entrada)")
+    escolha = input("> ").strip()
+
+    if escolha == "1":
+        modo_automatico()
+    else:
+        modo_manual()
+
+              # close the connection
